a0a58e12cf052ba64a85fb23cd17ad4dfc77ba70
Serpil Orak <serpilorak@192.168.1.112>
2026-08-27 22:47:56 +0300

    feat: add duration selector options and bone reward reset mechanism

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ const pauseBtn = document.getElementById("pause-btn");
 const resetBtn = document.getElementById("reset-btn");
 const dogRunner = document.getElementById("dog-runner");
 const boneCountDisplay = document.getElementById("bone-count");
+const timeButtons = document.querySelectorAll(".time-btn[data-time]");
+const customMinutesInput = document.getElementById("custom-minutes");
+const setCustomBtn = document.getElementById("set-custom-btn");
 
 // Zaman ve durum değişkenleri
-const TOTAL_TIME = 5; //25 * 60; // 25 dakika = 1500 saniye
+let TOTAL_TIME = 25 * 60; // 25 dakika = 1500 saniye
 let timeLeft = TOTAL_TIME; // kalan saniye
 let timerInterval = null; // Zaman sayacını çalıştırıcak saat mekanizmaası
 let isRunning = false; //Sayaç şuan akıyor mu
@@ -51,6 +54,13 @@ function startTimer() {
       isRunning = false;
       completedWalks++; // Kemik sayısını 1 artır
 
+      //Köpeği tekrardan başlangıç noktasına döndürür
+      dogRunner.style.left = "0%";
+
+      //Süreyi sıfırlar
+      timeLeft = TOTAL_TIME;
+      updateDisplay();
+
       statusText.textContent = "Tebrikler! Köpeğin parka vardı";
       boneCountDisplay.textContent = `🦴 x ${completedWalks}`;
       alert("Harika bir odaklanma seansı! Köpeğin parka ulaştı");
@@ -83,3 +93,42 @@ resetBtn.addEventListener("click", resetTimer);
 
 // Sayfa iilk açıldığında sayacı ekrana düzgün yazdıralım
 updateDisplay();
+
+// Seçilen süreyi ayarlayan genel fonksiyon
+function setDuration(minutes) {
+  //Eğer sayaç zaten çalışıyorsa durdur
+  clearInterval(timerInterval);
+  isRunning = false;
+  //Dakikayı saniyeye çevirip değişkenlere ata
+  TOTAL_TIME = minutes * 60;
+  timeLeft = TOTAL_TIME;
+
+  //Ekranı güncelle ve köpeği başa al
+  updateDisplay();
+  dogRunner.style.left = "0%";
+  statusText.textContent = `${minutes} dakikalık yürüyüş seçildi. Başlamaya hazır!`;
+}
+//BUtonlara tıklanınca
+timeButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    //aktif buton rengini güncelle
+    timeButtons.forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    const selectedMinutes = parseInt(btn.getAttribute("data-time"));
+    setDuration(selectedMinutes);
+  });
+});
+// Özel dakika girişi butonuna tıklanınca
+setCustomBtn.addEventListener("click", () => {
+  const enteredMinutes = parseInt(customMinutesInput.value);
+
+  if (!enteredMinutes || enteredMinutes <= 0) {
+    alert("Lütfen geçerli bir dakika giriniz!");
+    return;
+  }
+  //Butonlardaki aktifliği kaldır
+  timeButtons.forEach((b) => b.classList.remove("active"));
+  setDuration(enteredMinutes);
+  customMinutesInput.value = "";
+});
